007319b69d879f37f5c9adf70acd80e72886c942
CodeForGhost <ilikeabdul7@gmail.com>
2023-11-11 11:26:05 +0530

    add product page done

---
+++ src/app/add-product/page.tsx
@@ -0,0 +1,69 @@
+import prisma from "@/lib/db/primsa";
+import { redirect } from "next/navigation";
+import FormSubmitButton from "../components/FormSubmitButton";
+
+export const metadata = {
+  title: "Add Product - Ecommerce",
+};
+
+async function addProduct(formData: FormData) {
+  "use server";
+
+  const name = formData.get("name")?.toString();
+  const description = formData.get("description")?.toString();
+  const imageUrl = formData.get("imageUrl")?.toString();
+  const price = Number(formData.get("price"));
+
+  if (!name || !description || !imageUrl || !price) {
+    throw Error("Missing required fields");
+  }
+
+  await prisma.product.create({
+    data: {
+      name,
+      description,
+      imageUrl,
+      price,
+    },
+  });
+
+  redirect("/");
+}
+
+export default function AddProductPage() {
+  return (
+    <div>
+      <h1 className="mb-3 text-lg font-bold">Add Product</h1>
+      <form action={addProduct}>
+        <input
+          required
+          type="text"
+          name="name"
+          placeholder="Name"
+          className="input input-bordered input-primary mb-3 w-full"
+        />
+        <textarea
+          required
+          name="description"
+          placeholder="Description"
+          className="textarea textarea-bordered textarea-primary mb-3 w-full"
+        ></textarea>
+        <input
+          required
+          type="url"
+          name="imageUrl"
+          placeholder="Image Url"
+          className="input input-bordered input-primary mb-3 w-full"
+        />
+        <input
+          required
+          type="number"
+          name="price"
+          placeholder="Price"
+          className="input input-bordered input-primary mb-3 w-full"
+        />
+        <FormSubmitButton className="btn-block">Add Product</FormSubmitButton>
+      </form>
+    </div>
+  );
+}
